732c82f251b84f9064d3edac06837cbaec64219b
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-11-12 15:09:10 +0500

    fix Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import News from "./components/News";
-import Testimonal from "./components/Testimonal";
-import Contact from "./components/Contact";
-import Shop from "./components/Shop";
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import News from './components/News'
+import Testimonal from './components/Testimonal'
+import Contact from './components/Contact'
+import Shop from './components/Shop'
 
 const App = () => {
-  return (
-    <div style={{background:"linear-gradient(rgb(72, 138, 249) 0%, rgb(81, 163, 254) 100%)"}}>
-      <Navbar />
-      
-      <Hero />
-      <About />
-      <News />
-      <Testimonal />
-      <Contact />
-      <Shop />
-    </div>
-  );
-};
+	return (
+		<div
+			style={{
+				background:
+					'linear-gradient(rgb(72, 138, 249) 0%, rgb(81, 163, 254) 100%)',
+			}}
 
-export default App;
+      className='relative'
+		>
+			<Navbar />
+			<main>
+				<Hero />
+				<About />
+				<News />
+				<Testimonal />
+				<Contact />
+				<Shop />
+			</main>
+		</div>
+	)
+}
+
+export default App
